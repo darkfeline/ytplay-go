538a14e5e0901eea45fd90e22b5b456c2cc90fb1
Allen Li <darkfeline@felesatra.moe>
2020-02-06 00:01:15 -0800

    Work on unified stream

---
+++ fifo.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+// bufferedFIFO is a buffered FIFO.
+// This is needed because mpv needs a path to load.
+// mpv also does not do prefetch well, so passing a FIFO
+// directly between mpv and youtube-dl means that youtube-dl will only
+// start buffering once mpv gets to it.
+type bufferedFIFO struct {
+	pw *io.PipeWriter
+	bw *bufio.Writer
+}
+
+func (b *bufferedFIFO) Write(p []byte) (n int, err error) {
+	return b.bw.Write(p)
+}
+
+func (b *bufferedFIFO) Close() error {
+	_ = b.bw.Flush()
+	return b.pw.Close()
+}
+
+func newBufferedFIFO(path string) (*bufferedFIFO, error) {
+	if err := unix.Mkfifo(path, 0666); err != nil {
+		return nil, err
+	}
+	r, w := io.Pipe()
+	b := &bufferedFIFO{
+		pw: w,
+		bw: bufio.NewWriter(w),
+	}
+	go func() {
+		f, err := os.OpenFile(path, os.O_WRONLY, 0666)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		defer f.Close()
+		log.Printf("Writing to FIFO %s", path)
+		io.Copy(f, r)
+	}()
+	return b, nil
+}
--- ytplay.go
+++ ytplay.go
@@ -3,69 +3,184 @@ package main
 import (
 	"bufio"
 	"context"
-	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"sync"
+	"time"
 
 	"golang.org/x/sys/unix"
 )
 
-var (
-	video = flag.Bool("video", false, "Enable video")
-)
-
 func main() {
-	flag.Parse()
-	streams := make(chan io.Reader, 2)
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			url := scanner.Text()
-			fmt.Println(url)
-			r, err := bufferStream(ctx, url)
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-			streams <- r
-		}
-		if err := scanner.Err(); err != nil {
-			log.Print(err)
-		}
-		close(streams)
-		wg.Done()
-	}()
-	go func() {
-		for r := range streams {
-			playStream(r)
-		}
-		wg.Done()
-	}()
-	wg.Wait()
+	if err := innerMain(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func bufferStream(ctx context.Context, url string) (io.Reader, error) {
-	cmd := exec.Command("youtube-dl", "-q", "-o", "-", url)
-	cmd.Stderr = os.Stderr
-	r, err := cmd.StdoutPipe()
+func innerMain() error {
+	tmpdir, err := ioutil.TempDir("", "ytplay")
 	if err != nil {
-		return nil, err
+		return err
 	}
+	defer os.RemoveAll(tmpdir)
+	log.Printf("Using tmpdir %s", tmpdir)
+
+	files := make(chan string)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// TODO: Handle restarting if mpv exits
+		socket := filepath.Join(tmpdir, "socket")
+		f, ok := <-files
+		if !ok {
+			return
+		}
+		s, err := newMPVServer(socket, f)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		defer s.Close()
+		// TODO: Wait for socket to exist.
+		time.Sleep(time.Second)
+		c, err := newMPVClient(socket)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		go c.handleOutput()
+		defer c.Close()
+
+		for f := range files {
+			log.Printf("Appending mpv file %s", f)
+			c.appendFile(f)
+		}
+	}()
+
+	m := &streamManager{tmpdir: tmpdir}
+	defer m.Wait()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	_ = bufferURLs(ctx, m, os.Stdin, files)
+	close(files)
+	defer wg.Wait()
+	return nil
+}
+
+type mpvServer struct {
+	cmd  *exec.Cmd
+	wait chan struct{}
+}
+
+func newMPVServer(socket string, file string) (*mpvServer, error) {
+	args := []string{"mpv", "--no-config", "--no-terminal", "--force-window=immediate",
+		"--cache=yes", "--cache-secs=600",
+		"--keep-open=yes", "--input-ipc-server=" + socket, file}
+	log.Printf("Starting mpv with %v", args)
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stderr = os.Stderr
 	if err := cmd.Start(); err != nil {
 		return nil, err
 	}
+	s := &mpvServer{
+		cmd:  cmd,
+		wait: make(chan struct{}),
+	}
+	go func() {
+		_ = s.cmd.Wait()
+		log.Printf("mpv exited")
+		close(s.wait)
+	}()
+	return s, nil
+}
+
+func (s *mpvServer) Close() error {
+	_ = s.cmd.Process.Signal(unix.SIGTERM)
+	<-s.wait
+	return nil
+}
+
+type mpvClient struct {
+	net.Conn
+}
+
+func newMPVClient(socket string) (*mpvClient, error) {
+	conn, err := net.Dial("unix", socket)
+	if err != nil {
+		return nil, err
+	}
+	return &mpvClient{
+		Conn: conn,
+	}, nil
+}
+
+func (c *mpvClient) appendFile(f string) {
+	b := []byte(fmt.Sprintf(`{"command": ["loadfile", "%s", "append-play"]}
+`, f))
+	_, _ = c.Conn.Write(b)
+}
+
+func (c *mpvClient) handleOutput() {
+	io.Copy(ioutil.Discard, c.Conn)
+}
+
+func bufferURLs(ctx context.Context, m *streamManager, r io.Reader, files chan<- string) error {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		url := scanner.Text()
+		fmt.Println(url) // TODO: factor this out
+		log.Printf("Buffering stream %s", url)
+		path, err := m.addStream(ctx, url)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		log.Printf("Got streaming FIFO %s for %s", path, url)
+		files <- path
+	}
+	return scanner.Err()
+}
+
+type streamManager struct {
+	tmpdir string
+	last   int
+	wg     sync.WaitGroup
+}
+
+func (m *streamManager) Wait() {
+	m.wg.Wait()
+}
+
+func (m *streamManager) addStream(ctx context.Context, url string) (string, error) {
+	path := m.nextFIFO()
+	b, err := newBufferedFIFO(path)
+	if err != nil {
+		return "", err
+	}
+	// TODO: Clean up FIFOs somewhere.
+	cmd := exec.Command("youtube-dl", "-q", "-o", "-", url)
+	cmd.Stdout = b
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		b.Close()
+		return "", err
+	}
+	log.Printf("Started youtube-dl for %s to %s", url, path)
 	done := make(chan struct{})
+	m.wg.Add(2)
 	go func() {
 		_ = cmd.Wait()
+		log.Printf("youtube-dl for %s exited", url)
+		_ = b.Close()
 		close(done)
+		m.wg.Done()
 	}()
 	go func() {
 		select {
@@ -73,18 +188,12 @@ func bufferStream(ctx context.Context, url string) (io.Reader, error) {
 			_ = cmd.Process.Signal(unix.SIGTERM)
 		case <-done:
 		}
+		m.wg.Done()
 	}()
-	return r, nil
+	return path, nil
 }
 
-func playStream(r io.Reader) {
-	var cmd *exec.Cmd
-	if *video {
-		cmd = exec.Command("mpv", "--no-terminal", "-")
-
-	} else {
-		cmd = exec.Command("mpv", "--no-terminal", "--no-video", "-")
-	}
-	cmd.Stdin = r
-	_ = cmd.Run()
+func (m *streamManager) nextFIFO() string {
+	m.last++
+	return filepath.Join(m.tmpdir, fmt.Sprintf("fifo%d", m.last))
 }
